7d327da822c4e5e70e1d1a39d7486b197ffa590a
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-10-22 11:12:57 +0300

    Updates

---
+++ precache-manifest.a3cbf309e3577d1deef18cf952d4653e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156be655eda0dd6f5c5fbc402c6a5a25",
+    "url": "/currency_exchange/index.html"
+  },
+  {
+    "revision": "0832e3aa520c5e422db6",
+    "url": "/currency_exchange/static/css/main.bb4ebea3.chunk.css"
+  },
+  {
+    "revision": "3669f6f7252c36806f41",
+    "url": "/currency_exchange/static/js/4.7ed11c3c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/currency_exchange/static/js/4.7ed11c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443fb79016aa0037fc00",
+    "url": "/currency_exchange/static/js/5.41247c7c.chunk.js"
+  },
+  {
+    "revision": "4c42562c309488b48c7a",
+    "url": "/currency_exchange/static/js/currency-page.28797b0f.chunk.js"
+  },
+  {
+    "revision": "ee092096e4ecd5e1859c",
+    "url": "/currency_exchange/static/js/home-page.cfc07b74.chunk.js"
+  },
+  {
+    "revision": "0832e3aa520c5e422db6",
+    "url": "/currency_exchange/static/js/main.0a2a7ea2.chunk.js"
+  },
+  {
+    "revision": "1369d5bfc40d29a6d5a8",
+    "url": "/currency_exchange/static/js/runtime-main.7d29ca78.js"
+  }
+]);
