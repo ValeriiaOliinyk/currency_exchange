e18a0883f5eb87bacde92a7d886c548041cea184
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-11-02 15:49:56 +0200

    deleted test from Convert component

--- src/components/Convert/Convert.test.jsx
+++ src/components/Convert/Convert.test.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import "jest-styled-components";
-// import { Provider } from "react-redux";
-// import store from "../../redux/store";
-
-// import { CurrencyRow } from "../CurrencyRow/CurrencyRow";
 import { Container, Title } from "../../styled";
 
 import theme from "../../styles/theme";
-
-// it("CurrencyRow component works", () => {
-//   const tree = renderer
-//     .create(
-//       <Provider store={store.store}>
-//         <CurrencyRow theme={theme} />
-//       </Provider>
-//     )
-//     .toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
 
 describe("Convert component renders correctly", () => {
   it("Container component works", () => {
