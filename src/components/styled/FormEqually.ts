a130b01c13dcc4307a760486aea7772b954f06a5
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-11-08 15:21:53 +0200

    add another url

--- src/components/styled/FormEqually.ts
+++ src/components/styled/FormEqually.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FormEqually = styled.div`
   width: 40px;
   height: 40px;
-  background-image: url(${`${window.location.origin}/images/equal.svg`});
+  background-image: url("./images/equal.svg");
   text-align: center;
   font-size: 30px;
   margin-bottom: 26px;
